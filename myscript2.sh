cd7025280a50c144960fde7007699c5fad804308
[fatmaayhan-uf] <[fayhan@ufl.edu]>
2014-09-25 20:47:40 -0400

    [commit2]

--- myscript2.sh
+++ myscript2.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 for planet in Mercury Venus Earth Mars Jupiter Uranus Neptun
-do curl "http://en.wikipedia.org/wiki/$planet" > $planet.txt 
-   git add $planet.txt 
-   git commit -m "[assign1 commit1]"
+do curl "http://en.wikipedia.org/wiki/$planet" > $planet.txt
+if [ -s $planet.txt ]
+then
+	git add $planet.txt
+	git commit -m "[assign1 commit1]"
+else
+	echo "$planet is empty"
+fi
 done
